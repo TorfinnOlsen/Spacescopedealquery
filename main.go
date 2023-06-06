eff12e67381445d5be48c1428e46ab0372268502
TorfinnOlsen <torfinn.olsen@protocol.ai>
2023-06-06 14:38:07 -0700

    update to improved scope

--- main.go
+++ main.go
@@ -4,73 +4,76 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-type Response struct {
-	RequestID string `json:"request_id"`
-	Code      int    `json:"code"`
-	Message   string `json:"message"`
-	Data      []Deal `json:"data"`
+type Deal struct {
+	StatDate                            string `json:"stat_date"`
+	DailyProposedRegularDealCount       int    `json:"daily_proposed_regular_deal_count"`
+	DailyProposedVerifiedDealCount      int    `json:"daily_proposed_verified_deal_count"`
+	DailyActivatedRegularDealCount      int    `json:"daily_activated_regular_deal_count"`
+	DailyActivatedVerifiedDealCount     int    `json:"daily_activated_verified_deal_count"`
+	DailySlashedRegularDealCount        int    `json:"daily_slashed_regular_deal_count"`
+	DailySlashedVerifiedDealCount       int    `json:"daily_slashed_verified_deal_count"`
+	DailyExpiredRegularDealCount        int    `json:"daily_expired_regular_deal_count"`
+	DailyExpiredVerifiedDealCount       int    `json:"daily_expired_verified_deal_count"`
+	TotalRegularDealCount               int    `json:"total_regular_deal_count"`
+	TotalVerifiedDealCount              int    `json:"total_verified_deal_count"`
+	ActiveRegularDealCount              int    `json:"active_regular_deal_count"`
+	ActiveVerifiedDealCount             int    `json:"active_verified_deal_count"`
 }
 
-type Deal struct {
-	StatDate                        string `json:"stat_date"`
-	DailyProposedRegularDealCount   int    `json:"daily_proposed_regular_deal_count"`
-	DailyProposedVerifiedDealCount  int    `json:"daily_proposed_verified_deal_count"`
-	DailyActivatedRegularDealCount  int    `json:"daily_activated_regular_deal_count"`
-	DailyActivatedVerifiedDealCount int    `json:"daily_activated_verified_deal_count"`
-	DailySlashedRegularDealCount    int    `json:"daily_slashed_regular_deal_count"`
-	DailySlashedVerifiedDealCount   int    `json:"daily_slashed_verified_deal_count"`
-	DailyExpiredRegularDealCount    int    `json:"daily_expired_regular_deal_count"`
-	DailyExpiredVerifiedDealCount   int    `json:"daily_expired_verified_deal_count"`
-	TotalRegularDealCount           int    `json:"total_regular_deal_count"`
-	TotalVerifiedDealCount          int    `json:"total_verified_deal_count"`
-	ActiveRegularDealCount          int    `json:"active_regular_deal_count"`
-	ActiveVerifiedDealCount         int    `json:"active_verified_deal_count"`
+type Response struct {
+	Data []Deal `json:"data"`
 }
 
 func main() {
-	spacescopeAPIKey := os.Getenv("SPACESCOPE_API_KEY")
-	if spacescopeAPIKey == "" {
-		log.Fatal("SPACESCOPE_API_KEY environment variable is not set")
-	}
-
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.spacescope.io/v2/deals/deal_count", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	q := req.URL.Query()
-	q.Add("start_date", time.Now().AddDate(0, 0, -7).Format("2006-01-02"))
-	q.Add("end_date", time.Now().Format("2006-01-02"))
-	req.URL.RawQuery = q.Encode()
-
-	req.Header.Add("Authorization", "Bearer "+spacescopeAPIKey)
+	req, _ := http.NewRequest("GET", "https://api.spacescope.io/v2/deals/deal_count", nil)
+	query := req.URL.Query()
+	query.Add("start_date", time.Now().AddDate(0, 0, -7).Format("2006-01-02"))
+	query.Add("end_date", time.Now().Format("2006-01-02"))
+	req.URL.RawQuery = query.Encode()
+	req.Header.Add("Authorization", "Bearer "+os.Getenv("SPACESCOPE_API_KEY"))
 	resp, err := client.Do(req)
+
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error on response.\n[ERROR] -", err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var dealsResponse Response
+	json.Unmarshal(body, &dealsResponse)
+
+	var totalDailyProposedRegularDealCount, totalDailyProposedVerifiedDealCount int
+	var totalDailyActivatedRegularDealCount, totalDailyActivatedVerifiedDealCount int
+	var totalDailySlashedRegularDealCount, totalDailySlashedVerifiedDealCount int
+	var totalDailyExpiredRegularDealCount, totalDailyExpiredVerifiedDealCount int
+
+	for _, deal := range dealsResponse.Data {
+		totalDailyProposedRegularDealCount += deal.DailyProposedRegularDealCount
+		totalDailyProposedVerifiedDealCount += deal.DailyProposedVerifiedDealCount
+		totalDailyActivatedRegularDealCount += deal.DailyActivatedRegularDealCount
+		totalDailyActivatedVerifiedDealCount += deal.DailyActivatedVerifiedDealCount
+		totalDailySlashedRegularDealCount += deal.DailySlashedRegularDealCount
+		totalDailySlashedVerifiedDealCount += deal.DailySlashedVerifiedDealCount
+		totalDailyExpiredRegularDealCount += deal.DailyExpiredRegularDealCount
+		totalDailyExpiredVerifiedDealCount += deal.DailyExpiredVerifiedDealCount
 	}
 
-	var response Response
-	if err := json.Unmarshal(body, &response); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Total proposed regular deals:", totalDailyProposedRegularDealCount)
+	fmt.Println("Total proposed verified deals:", totalDailyProposedVerifiedDealCount)
+	fmt.Println("Total activated regular deals:", totalDailyActivatedRegularDealCount)
+	fmt.Println("Total activated verified dealsThe message appears to have been cut off, here is the remaining part of the code:
 
-	prettyJSON, err := json.MarshalIndent(response, "", "    ")
-	if err != nil {
-		log.Fatal("Failed to generate json", err)
-	}
-
-	fmt.Println(string(prettyJSON))
+```go
+	fmt.Println("Total activated verified deals:", totalDailyActivatedVerifiedDealCount)
+	fmt.Println("Total slashed regular deals:", totalDailySlashedRegularDealCount)
+	fmt.Println("Total slashed verified deals:", totalDailySlashedVerifiedDealCount)
+	fmt.Println("Total expired regular deals:", totalDailyExpiredRegularDealCount)
+	fmt.Println("Total expired verified deals:", totalDailyExpiredVerifiedDealCount)
 }
